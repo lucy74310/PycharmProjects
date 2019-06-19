3f7197afcf1ea8867efa853ffdfdf5c52f7774a4
lucy <lucy74310@gmail.com>
2019-06-19 15:52:37 +0900

    python_pgsql& python_ch3 ++

--- python_crawler/__main__.py
+++ python_crawler/__main__.py
@@ -1,11 +1,15 @@
 import ssl
 import sys
+import time
 from datetime import datetime
 from itertools import count
 from urllib.request import Request, urlopen
 
 import pandas as pd
 from bs4 import BeautifulSoup
+from selenium import webdriver
+
+from python_crawler.collection import crawler
 
 
 def crawling_pelicana():
@@ -108,6 +112,114 @@ def crawling_nene():
     table.to_csv('__results__/nene.csv', encoding='utf-8', mode='w', index=True)
 
 
+def ex01():
+    results = []
+
+    # count(시작점) 무한대로감, break 반드시 필요
+    for page in count(start=113):
+        url = 'https://pelicana.co.kr/store/stroe_search.html?branch_name=&gu=&si=&page=%d'%page
+        html = crawler.crawling(url)
+
+        bs = BeautifulSoup(html, 'html.parser')
+        tag_table = bs.find('table', attrs={'class':'table mt20'})
+        tag_tbody = tag_table.find('tbody')
+        tags_tr = tag_tbody.findAll('tr')
+
+        # 끝 검출
+        if len(tags_tr) == 0:
+            break
+
+        for tag_tr in tags_tr:
+            # tag 다 빼고 string 만
+            strings = list(tag_tr.strings)
+            name = strings[1]
+            address = strings[3]
+            sidogu = address.split()[:2]
+            t = (name, address)+tuple(sidogu)
+            results.append(t)
+
+    # store
+    table = pd.DataFrame(results, columns=['name', 'address', 'sido', 'gu'])
+    table.to_csv('__results__/pelicana.csv', encoding='utf-8', mode='w', index=True)
+
+    # for t in results:
+    #     print(t)
+
+
+def crawling_kyochon():
+    retults = []
+
+    for sido1 in range(1, 2):
+        for sido2 in count(25, 27):
+            url = 'http://www.kyochon.com/shop/domestic.asp?sido1=%d&sido2=%d&txtsearch=' % (sido1, sido2)
+            html = crawler.crawling(url)
+
+            #끝 검출
+            if html is None:
+                break
+
+            bs = BeautifulSoup(html, 'html.parser')
+            tag_ul = bs.find('ul', attrs={'class': 'list'})
+            tags_span = tag_ul.findAll('span', attrs={'class': 'store_item'})
+
+            for tag_span in tags_span:
+                strings = list(tag_span.strings)
+
+                name = strings[1]
+                address = strings[3].strip('\r\n\t')
+                sidogu = address.split()[:2]
+
+                retults.append((name, address) + tuple(sidogu))
+
+    for t in retults:
+        print(t)
+
+
+def crawling_goobne():
+    url = 'http://goobne.co.kr/store/search_store.jsp'
+    wd = webdriver.Chrome('D:\cafe24\chromedriver_win32\chromedriver.exe')
+    wd.get(url)
+
+    time.sleep(5)
+
+    results = []
+    for page in range(1, 106):
+        # 자바 스크립트 실행
+        script = 'store.getList(%d)' % page
+        wd.execute_script(script)
+        print(f'{datetime.now()}: success for request [{script}]')
+        time. sleep(3)
+
+        # 실행결과 HTML( 동적으로 렌더링 된 html) 가져오기
+        html = wd.page_source
+
+        # parsing with bs4
+        bs = BeautifulSoup(html, 'html.parser')
+        tag_tbody = bs.find('tbody', attrs={'id': 'store_list'})
+        tags_tr = tag_tbody.findAll('tr')
+
+        # detect last page
+        if tags_tr[0].get('class') is None:
+            break
+
+        for tag_tr in tags_tr:
+            strings = list(tag_tr.strings)
+            name = strings[1]
+            address = strings[6]
+            sidogu = address.split()[:2]
+
+            results.append((name, address) + tuple(sidogu))
+
+    wd.quit()
+
+    # store
+    table = pd.DataFrame(results, columns=['name', 'address', 'sido', 'gu'])
+    table.to_csv('__results__/goobne.csv', encoding='utf-8', mode='w', index=True)
+
+    for result in results:
+        print(result)
+
+
 if __name__ == '__main__':
     # pelicana
 
@@ -119,4 +231,6 @@ if __name__ == '__main__':
     # crawling_pelicana()
 
     #nene 과제
-    crawling_nene()
+    # ex01()
+    # crawling_kyochon()
+    crawling_goobne()
